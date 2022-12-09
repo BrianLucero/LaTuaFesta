9b9fa159c6eed0dc95ea6f7fc258c239894b26d5
Brian Lucero <lucero14.lucero@gmail.com>
2022-12-08 21:56:59 -0300

    CAMBIOS NUEVOS LA TUA FESTA

--- js/index.js
+++ js/index.js
@@ -1,28 +1,12 @@
  function Mostrar() {
     document.getElementById("divCompleto").style.display = "none";   
-    document.getElementById("navMenu").style.display = "block";
-    //document.getElementById("navProd").style.display = "none";   
-}
+    document.getElementById("navMenu").style.display = "block"; 
+};
 
 function Cerrar() {
     document.getElementById("divCompleto").style.display = "block";   
-    document.getElementById("navMenu").style.display = "none";
-    //document.getElementById("producto").style.color = "white"; 
-   // document.getElementById("navProd").style.display = "none";   
-
-}
-/*
-function mostrarProd() {
-    document.getElementById("navProd").style.display = "block";
-    document.getElementById("producto").style.color = "blue";
-}
-*/
-
-/*unction cerrarProd() {
-   document.getElementById("navProd").style.display = "none";
-
-}*/
-
+    document.getElementById("navMenu").style.display = "none"; 
+};
 
 const irArriba = document.querySelector(".ir-arriba");
 
